c0bef59ad4f1442468b52e0ad4e27c230fc185f5
Lovell Fuller <github@lovell.info>
2016-02-22 10:05:38 +0000

    Improve Sobel operator in edge mask
    Improve performance of colour mask
    
    Add training model for skin-tone mask

--- src/mask.cc
+++ src/mask.cc
@@ -10,28 +10,22 @@ vips::VImage Mask::Edges(vips::VImage input) {
   // Convert image to greyscale and apply small blur
   vips::VImage grey = input.colourspace(VIPS_INTERPRETATION_B_W).gaussblur(2.0);
 
-  // Create horizontal operator
-  vips::VImage sobelX = vips::VImage::new_matrixv(3, 3,
+  // Create Sobel operator
+  vips::VImage sobel = vips::VImage::new_matrixv(3, 3,
     -1.0, 0.0, 1.0,
     -2.0, 0.0, 2.0,
     -1.0, 0.0, 1.0);
-  // Create vertical operator
-  vips::VImage sobelY = vips::VImage::new_matrixv(3, 3,
-    1.0, 2.0, 1.0,
-    0.0, 0.0, 0.0,
-    -1.0, -2.0, -1.0);
 
-  // Apply Sobel operators
-  vips::VImage sobelFiltered = grey.conv(sobelX) + grey.conv(sobelY);
+  // Apply horizontal and vertical Sobel operators
+  vips::VImage sobelFiltered =
+    (grey.conv(sobel) - 128).abs() +
+    (grey.conv(sobel.rot90()) - 128).abs();
 
-  // Halve range to stay within 0-255
-  sobelFiltered = (sobelFiltered / 2).cast(VIPS_FORMAT_UCHAR);
+  // Calculate value threshold at which we can keep 15% of pixels
+  const double sobelThreshold = static_cast<double>(sobelFiltered.percent(15.0));
 
-  // Calculate value threshold at which we can discard 85% of pixels
-  const double sobelThreshold = static_cast<double>(sobelFiltered.percent(85.0));
-
-  // Remove pixels below threshold
-  return sobelFiltered >= sobelThreshold;
+  // Keep pixels below threshold
+  return sobelFiltered < sobelThreshold;
 };
 
 /*
@@ -40,15 +34,15 @@ vips::VImage Mask::Edges(vips::VImage input) {
 vips::VImage Mask::Colours(vips::VImage input) {
 
   // Apply Gaussian blur
-  vips::VImage blurred = input.gaussblur(1.0);
+  vips::VImage lab = input.colourspace(VIPS_INTERPRETATION_LAB);
 
   // Generate image containing the average colour
-  const int shrunkWidth = input.width();
-  const int shrunkHeight = input.height();
-  vips::VImage averageColour = blurred.shrink(shrunkWidth, shrunkHeight).colourspace(VIPS_INTERPRETATION_LAB).zoom(shrunkWidth, shrunkHeight);
+  const int shrunkWidth = lab.width();
+  const int shrunkHeight = lab.height();
+  vips::VImage averageColour = lab.shrink(shrunkWidth, shrunkHeight).zoom(shrunkWidth, shrunkHeight);
 
   // Calculate Delta-E 2000 distance to the average in the LAB colour space
-  vips::VImage averageDelta = blurred.colourspace(VIPS_INTERPRETATION_LAB).dE00(averageColour);
+  vips::VImage averageDelta = lab.dE00(averageColour);
 
   // Calculate value threshold at which we can discard 85% of pixels
   const double colourThreshold = static_cast<double>(averageDelta.percent(85.0));
@@ -61,6 +55,6 @@ vips::VImage Mask::Colours(vips::VImage input) {
   Generate combined saliency mask
 */
 vips::VImage Mask::Saliency(vips::VImage input) {
-  // Keep pixels that appear in both masks and remove noise with 5x5 median filter
-  return (Edges(input) & Colours(input)).rank(5, 5, 12);
+  // Keep pixels that appear in both masks
+  return (Edges(input) & Colours(input));
 };
